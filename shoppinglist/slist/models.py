8c6bc74da1eac7bcfda54d7695f25ec5790012d0
OlexiyKi <olexiykirsanov@gmail.com>
2023-02-15 20:42:47 +0200

    add models and migration

--- shoppinglist/slist/models.py
+++ shoppinglist/slist/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class ShoppingList(models.Model):
+    list_id = models.UUIDField()
+    list_id = models.ForeignKey('Item', on_delete=models.CASCADE)
+    item_id = models.IntegerField()
+    quantity = models.IntegerField()
+    price = models.DecimalField(decimal_places=2, max_digits=10, null=True)
+    status = models.CharField(default="available", max_length=20)
+    buy_day = models.DateField(null=True)
+
+
+class UserList (models.Model):
+    user_id = models.IntegerField()
+    list_id = models.UUIDField()
+
+
+class MallList(models.Model):
+    name = models.CharField(max_length=50)
+    list_id = models.UUIDField()
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    shop_id = models.ForeignKey(MallList, on_delete=models.CASCADE)
